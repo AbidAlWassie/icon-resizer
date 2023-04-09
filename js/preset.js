06d17bbca041fc5ef509a59e926c607a74007396
Abid Al Wassie <abidalwassie@outlook.com>
2023-04-09 02:07:14 +0000

    resizing images based on width & height

--- js/preset.js
+++ js/preset.js
@@ -2,18 +2,20 @@ inputField = document.getElementById("inputField");
 addBtn = document.getElementById("addBtn");
 resizeOptions = document.getElementById("resizeOptions");
 
-function addElements(numElements) {
-  for (i = 0; i < numElements; i++) {
-    newElement = document.createElement("div");
-    newElement.innerHTML = `
-    <input class="resizeUnit" type="number" value="${
-      i + 1
-    }">x<input class="resizeUnit" type="number" value="${i + 1}">`;
-    resizeOptions.appendChild(newElement);
-  }
-}
-
 addBtn.addEventListener("click", function () {
   numElements = parseInt(inputField.value);
   addElements(numElements);
 });
+
+function addElements(numElements) {
+  for (i = 0; i < numElements; i++) {
+    newElement = document.createElement("div");
+    newElement.innerHTML = `
+    <input class="resizeUnit resizer__input resizer__input--width" type="number" placeholder="${
+      i + 1
+    }">x<input class="resizeUnit resizer__input resizer__input--width" type="number" placeholder="${
+      i + 1
+    }">`;
+    resizeOptions.appendChild(newElement);
+  }
+}
