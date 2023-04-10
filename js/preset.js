6c68abed593c3f782e413cae6f4f7e9b1681ddc0
Abid Al Wassie <abidalwassie@outlook.com>
2023-04-10 14:10:59 +0000

    applying values according to selected preset

--- js/preset.js
+++ js/preset.js
@@ -1,6 +1,10 @@
-inputField = document.getElementById("inputField");
-addBtn = document.getElementById("addBtn");
-resizeOptions = document.getElementById("resizeOptions");
+window.onload = function () {
+  loadJsonData();
+};
+
+var inputField = document.getElementById("inputField");
+var addBtn = document.getElementById("addBtn");
+var resizeOptions = document.getElementById("resizeOptions");
 
 const iconsSelect = document.getElementById("icons");
 
@@ -10,15 +14,36 @@ function loadJsonData() {
   const selectedValue = iconsSelect.value;
   const fileName = `${selectedValue}.json`;
 
+  if (!resizeOptions.innerHTML.trim() === "") {
+    console.log("empty");
+    resizeOptions.innerHTML = "";
+  } else {
+    resizeOptions.innerHTML = "";
+    console.log("not empty");
+  }
+
   fetch(`json/${fileName}`)
     .then((response) => response.json())
     .then((data) => {
       const counter = data.sizes.length;
-      // console.log(data);
       console.log(`Number of objects: ${counter}`);
+
       for (i = 0; i < counter; i++) {
         const obj = data.sizes[i];
+        const newInput = document.createElement("div");
+        newInput.className = "resizeUnits";
+
+        newInput.innerHTML = `
+        <input class="resizeUnit width" type="number" placeholder="${
+          i + 1
+        }" value=${
+          obj.width
+        }>x<input class="resizeUnit height" type="number" placeholder="${
+          i + 1
+        }" value=${obj.height}>`;
+
         console.log(`${i}: ${obj.name}, ${obj.width}, ${obj.height}`);
+        resizeOptions.appendChild(newInput);
       }
     })
     .catch((error) => {
